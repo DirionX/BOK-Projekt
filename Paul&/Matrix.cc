52ead233d541fc82a83e7c4f4049732969d980a5
Pjascha <136508428+Pjascha@users.noreply.github.com>
2023-07-05 18:31:16 +0200

    Add files via upload

--- Paul&/Matrix.cc
+++ Paul&/Matrix.cc
@@ -4,12 +4,12 @@
 // Definitionen für die Matrix Klasse
 
 // Construktor Definition, ist etwas fancy und verwirrend aber funktioniert hoffentlich.
-Matrix::Matrix(int n, int m) : M(m), N(n){
+Matrix::Matrix(int m, int n) : M(m), N(n){
     // n sind Spalten; m sind Reihen (glaub ich zumindest)
-    matrix = new double*[n];
-    for (int i = 0; i < n; i++){
-        matrix[i] = new double[m];
-        for (int j = 0; j < m; j ++) {
+    matrix = new double*[M];
+    for (int i = 0; i < M; i++){
+        matrix[i] = new double[N];
+        for (int j = 0; j < N; j ++) {
             double eintrag = 0;
             matrix[i][j] = eintrag;
         }
@@ -28,9 +28,6 @@ Matrix::Matrix(int n):  M(n), N(n){
         }
     }
 }
-
-
-
 
 
 Matrix::~Matrix() {
@@ -55,7 +52,6 @@ Matrix Matrix::transposition() {
     Matrix transp(this->N, this->M);
     for (int i = 0; i < this->N; i++) {
         for (int j = 0; j < this->M; j++) {
-            std::cout << i << " " << j <<std::endl;
             transp[i][j] = (*this)[j][i];
         }
     }
@@ -64,10 +60,10 @@ Matrix Matrix::transposition() {
 
 void Matrix::show(){
     //Mega ätzend zu printen...
-    for (int i = 0; i<N; i++){
+    for (int i = 0; i<M; i++){
         std::cout << "[";
-        for (int j = 0; j<M; j++){
-            if (j == M - 1){
+        for (int j = 0; j<N; j++){
+            if (j == N - 1){
                 std::cout << matrix[i][j];
             }else{
                 std::cout << matrix[i][j] << ",";
--- Paul&/source.cc
+++ Paul&/source.cc
@@ -3,9 +3,12 @@
 
 int main(){
     // verstehe ich jetzt auch nicht mehr um ehrlich zu sein... Funktioniert aber.
+    Matrix z_vec(3, 1);
+    z_vec.show();
+    Matrix s_vec = z_vec.transposition();
+    s_vec.show();
     Matrix mat3x3(3,3);
     mat3x3.show();
-    std::cout <<mat3x3[0][0] << std::endl;
     double new_line[3] = {1, 2, 3};
     mat3x3[0] = new_line;
     mat3x3[1] = new_line;
